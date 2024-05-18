eea51adab77cc70b7705f8578bc28a3ff26831ed
sarahd <sarahjdavila210@gmail.com>
2024-05-18 00:39:11 -0700

    updated About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
                 <h2>About Me</h2>
                 <img src={sarahImage} alt="picture of Sarah Davila" className="photo" />
                 <p id="paragraph">
-                    My name is Sarah Davila, and I am a front-end web developer based in Las Vegas, NV. I became interested in web
+                    My name is Sarah Davila, and I am a web developer based in Las Vegas, NV. I became interested in web
                     development during the pandemic, and I am currently taking a bootcamp to develop and sharpen my skills in
                     different languages such as HTML, CSS, and JavaScript.
                 </p>
